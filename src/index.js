c15102c851b058c77fd04500da84ce7132e1d45c
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-11 20:58:26 -0700

    add working API call
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './css/styles.css';
 import [] from './js/[].js';
 
 // Business Logic
-
+ 
 function getPokemon(name) {
-  // this is where we will build our function to call the API and return a random pokemon name.
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+  const url = `https://pokeapi.co/api/v2/pokemon/{###}/`;
+
+  request.addEventListener("loadend", function () {
+    const pokeNames = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      resolve(pokeNames);
+    } else {
+      reject([this, pokeNames]);
+    }
+  });
+  request.open("GET", url, true);
+  request.send();
+  });
+}
+
+
+// U I Logic
