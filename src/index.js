bfc2a95263aafc6b9ffc3d005bb6b7a07fbfcf63
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-12 20:45:41 -0700

    WIP commit troubleshooting UI logic interaction with API call
    Co-authored-by: Najma A <anajmabdi@users.noreply.github.com>
    Co-authored-by: OLGON92 <OLGON92@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import [] from './js/[].js';
+// import [] from './js/[].js';
 
 // Business Logic
  
-function getPokemon(name) {
+function getPokemon(inputtedNumber) {
   let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-  const url = `https://pokeapi.co/api/v2/pokemon/{###}/`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${inputtedNumber}/`;
 
   request.addEventListener("loadend", function () {
     const pokeNames = JSON.parse(this.responseText);
@@ -25,3 +25,14 @@ function getPokemon(name) {
 
 // U I Logic
 
+function handleUserNumber(event) {
+  event.preventDefault();
+  const  inputtedNumber = parseInt(document.querySelector("input#get-pokemon").value);
+  getPokemon(inputtedNumber);
+}
+console.log(handleUserNumber(5));
+
+function printCall(pokeNames) {
+  document.querySelector('#showResponse').innerText = `${pokeNames}`;
+}
+console.log(printCall());
